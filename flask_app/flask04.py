e34f129fb4a0b683a99be4810e6a559cc9bd7a4d
Autumn Horrell <ahorrell@uncc.edugit config -- global core.autocrlf inputgit config --global core.safecrlf warncd Downloads/git_tutorial/cdDownloads/git_tutorial.zip/git_tutorial/git config --global user.email ahorrell@uncc.edu>
2021-11-02 09:46:06 -0400

    flask app #4 - update notes view to use list from db

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -17,54 +17,55 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db.init_app(app)
 # Setup models
 with app.app_context():
-    db.create_all()   # run under the app context
+	db.create_all()   # run under the app context
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 
+@app.route('/')
 @app.route('/index')
 def index():
-	
-    a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
-	
-    return render_template("index.html" , user = a_user)
+
+	a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
+
+	return render_template("index.html", user=a_user)
 
 @app.route('/notes')
 def get_notes():
 
-    a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
+	a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
 
-    my_notes = db.session.query(Note).all()
+	my_notes = db.session.query(Note).all()
 
-    return render_template('notes.html', notes=notes, user = a_user)
+	return render_template('notes.html', notes=my_notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
 
-    a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
+	a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
 
-    my_note = db.session.query(Note).filter_by(id=note_id)
+	my_note = db.session.query(Note).filter_by(id=note_id)
 
-    return render_template('note.html', nose=my_note, user = a_user)
+	return render_template('note.html', nose=my_note, user=a_user)
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
-    
-    if request.method == 'POST':
-        title = request.form['title']
-        text = request.form['noteText']
-        from datetime import date
-        today = date.today()
-        today = today.strftime("%m-%d-%Y")
-        new_record = Note(title, text, today)
-        db.session.add(new_record)
-        db.session.commit()
 
-        return redirect(url_for('get_notes'))
-    else:
-	a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
-        return render_template('new.html', user = a_user)
+	if request.method == 'POST':
+		title = request.form['title']
+		text = request.form['noteText']
+		from datetime import date
+		today = date.today()
+		today = today.strftime("%m-%d-%Y")
+		new_record = Note(title, text, today)
+		db.session.add(new_record)
+		db.session.commit()
+
+		return redirect(url_for('get_notes'))
+	else:
+		a_user = db.session.query(User).filter_by(email='ahorrell@uncc.edu')
+		return render_template('new.html', user = a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
